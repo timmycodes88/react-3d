aa68c50dac1c58f0cbbefe76c641afa692109707
Tim <tim@brightsidedeveloper.com>
2022-12-17 17:13:09 -0600

    New Scene

--- src/Scenes/Scene.js
+++ src/Scenes/Scene.js
@@ -5,6 +5,7 @@ import {
   PerspectiveCamera,
 } from "@react-three/drei"
 import { Canvas, useFrame } from "@react-three/fiber"
+import { useEffect, useState } from "react"
 import Box from "../Objects/Box"
 import Car from "../Objects/Car"
 import Ground from "../Objects/Ground"
@@ -13,23 +14,49 @@ import Track from "../Objects/Track"
 import degreesToRadians from "../utils/degreesToRadians"
 
 export default function Scene() {
+  const [thirdPerson, setThirdPerson] = useState(false)
+  const [cameraPosition, setCameraPosition] = useState([-6, 3.9, 6.21])
+
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.key == "k") {
+        if (thirdPerson) {
+          setCameraPosition([-6, 3.9, 6.21 + Math.random() * 0.01])
+        }
+        setThirdPerson(!thirdPerson)
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyDown)
+    return () => window.removeEventListener("keydown", handleKeyDown)
+  }, [thirdPerson])
+
   return (
     <Body>
-      <OrbitControls target={[-2.64, -0.71, 0.03]} />
-
       <Environment
         files={process.env.PUBLIC_URL + "/textures/envmap.hdr"}
         background={"both"}
       />
 
-      <PerspectiveCamera makeDefault position={[-6, 3.9, 6.21]} fov={40} />
+      <PerspectiveCamera makeDefault position={cameraPosition} fov={40} />
+      {!thirdPerson && <OrbitControls target={[-2.64, -0.71, 0.03]} />}
 
-      <Car />
+      <Car thirdPerson={thirdPerson} />
       <Track />
       <Ground />
 
-      <ambientLight intensity={0.5} />
-      <spotLight position={[5, 10, 10]} />
+      <ambientLight intensity={0.2} />
+      <pointLight
+        position={[-2.86, 0.1, -0.9]}
+        intensity={0.1}
+        color={"deepskyblue"}
+      />
+      <pointLight
+        position={[-3.33, 0.1, -0.9]}
+        intensity={0.1}
+        color={"orange"}
+      />
+      <pointLight position={[0.41, 0.1, 2]} intensity={0.1} color="red" />
     </Body>
   )
 }
